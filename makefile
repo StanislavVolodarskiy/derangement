4c18dd6473def1fcf15f4614a6274aeceb1b2d58
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2025-12-15 02:03:05 +0300

    better mark

--- makefile
+++ makefile
@@ -15,3 +15,7 @@ setup:
 test:
 	flake8 derangement test* tools
 	PYTHONPATH=. pytest --pdb -v -s --cov=derangement --cov-report=term-missing .
+
+.PHONY: check_derangements
+check_derangements:
+	PYTHONPATH=. python tools/check_derangements.py
--- tools/check_derangements.py
+++ tools/check_derangements.py
@@ -1,4 +1,5 @@
 import collections
+import random
 import time
 
 import derangement
@@ -11,12 +12,45 @@ def elapsed(f):
     return r, finish - start
 
 
+class RandomCounter:
+    def __init__(self, seed):
+        self._r = random.Random(seed)
+        self._c = 0
+
+    def random(self):
+        self._c += 1
+        return self._r.random()
+
+    def randrange(self, *args):
+        self._c += 1
+        return self._r.randrange(*args)
+
+    def choice(self, a):
+        self._c += 1
+        return self._r.choice(a)
+
+    def shuffle(self, a):
+        self._c += len(a) - 1
+        self._r.shuffle(a)
+
+    def counter(self):
+        return self._c
+
+
 def test_function(n, k, label, f):
-    c, e = elapsed(lambda: collections.Counter(tuple(f(n)) for _ in range(k)))
+    r = RandomCounter(42)
+    c, e = elapsed(
+        lambda: collections.Counter(tuple(f(n, r)) for _ in range(k))
+    )
+
+    for a in c.keys():
+        assert all(i != ai for i, ai in enumerate(a))  # is derangement
+
     m = len(c)
     print(
         label,
         f'{e:5.2f}',
+        f'{r.counter():10d}',
         f'{k / m:10.2f}',
         [min(c.values()), '...', max(c.values())]
     )
@@ -24,27 +58,15 @@ def test_function(n, k, label, f):
 
 def test_all_functions(n, m):
     print('n =', n, 'm =', m)
-    test_function(
-        n,
-        m,
-        'derangement            ',
-        derangement.derangement
-    )
-    test_function(
-        n,
-        m,
-        'derangementFisherYates1',
-        derangement.derangementFisherYates1
-    )
-    test_function(
-        n,
-        m,
-        'derangementFisherYates2',
-        derangement.derangementFisherYates2
-    )
+    test_function(n, m, 'derangement ', derangement.derangement)
+    test_function(n, m, 'FisherYates1', derangement.derangementFisherYates1)
+    test_function(n, m, 'FisherYates2', derangement.derangementFisherYates2)
 
 
 def main():
+    for n in range(2, 11):
+        test_all_functions(n, 10_000_000)
+
     test_all_functions(10, 1_000_000)
     test_all_functions(100, 100_000)
     test_all_functions(1_000, 10_000)
@@ -52,8 +74,6 @@ def main():
     test_all_functions(100_000, 100)
     test_all_functions(1_000_000, 10)
     test_all_functions(10_000_000, 1)
-    for n in range(2, 11):
-        test_all_functions(n, 1_000_000)
 
 
 main()
